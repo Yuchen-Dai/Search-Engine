07d7b713c23cebdeeeed62b19917b3a1248044ee
Yuchen Dai <eason.yuchendai@gmail.com>
2019-11-09 19:26:18 -0800

    Update inverted_index

--- inverted_index.py
+++ inverted_index.py
@@ -1,15 +1,15 @@
 from pathlib import Path
-from collection import defaultdict
-import pandas as pd
+from collections import defaultdict
+import pickle
 import math
 
-def append_inverted(url, token_list, result):
+def append_inverted(stem, token_list, result):
 	tf_raw = defaultdict(int)
 	count = len(token_list)
 	for token in token_list:
 		tf_raw[token] += 1
 	for token, c in tf_raw.items():
-		result[token].append({'url':url,'tf': c/count,'idf': None,'tf-idf':None})
+		result[token].append({'stem':stem,'tf': c/count})
 
 
 def read_path(path):
@@ -17,34 +17,37 @@ def read_path(path):
 	return Path.iterdir(p)
 
 def read_text(file):
-	with Path.open() as f:
+	with file.open() as f:
 		url = f.readline()
-		pass
-	return url
+		text = list(map(lambda x: x.rstrip(), f.readlines()))
+	return text
 
 def calc_tf_idf(result, total_file):
 	for token in result:
 		count_token = len(result[token])
 		idf = math.log(total_file/(count_token + 1))
 		for document in result[token]:
-			document['idf'] = idf
-			document['tf_idf'] =  document['tf'] * idf
+			document['tf'] =  document['tf'] * idf
 
 def main():
-	result = defaultdict{list}
+	result = defaultdict(list)
 	#path of the source file
-	path = ''
+	path = 'op'
 	files = read_path(path)
 	
 	total_file = 0
 	for f in files:
 		total_file += 1
-		url, token_list = read_text(f)
-		append_inverted(url, token_list, result)
+		if total_file %1000 == 0 :
+			print(total_file)
+		token_list = read_text(f)
+		append_inverted(f.stem, token_list, result)
 
 	calc_tf_idf(result, total_file)
-	df = pd.DataFrame(result,index=[0])
-	df.to_csv('op.csv')
+	print(f'Number Docs: {total_file}')
+	print(f'Unique Words: {len(result)}')
+	with open('inverted_index.pkl', 'wb') as f:
+		pickle.dump(result, f, pickle.HIGHEST_PROTOCOL)
 
 if __name__ == "__main__":
 	main()
